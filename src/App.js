03c3ccacc1d12a0f3e89e5560f53de5374b0dece
root <root@DESKTOP-4OTEAIF.localdomain>
2021-10-05 10:45:41 -0300

    Adding ul, li and a elements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 const App = () =>{
   return(
-    <h1>Hello world</h1>
+    <>
+      <h1>Minha primeira aplicação com React</h1>
+      <ul>
+        <li>
+          <a href="/" className="list-group-item list-group-item-action list-group-item-dark">
+            Item 1
+          </a>
+        </li>
+        <li>
+          <a href="/" className="list-group-item list-group-item-action list-group-item-dark">
+            Item 2
+          </a>
+        </li>
+        <li>
+          <a href="/" className="list-group-item list-group-item-action list-group-item-dark">
+            Item 3
+          </a>
+        </li>
+      </ul>
+    </>    
   )
 }
 
---
+++ src/components/item/index.js
@@ -0,0 +1,9 @@
+const Item = (number) =>{
+    return (
+        <a href="/" className="list-group-item list-group-item-action list-group-item-dark">
+            Item {number}
+        </a>
+    )
+}
+
+export default Item;
